c23268841e0086f57724f90385871bff6bdcbf6c
BarbaraRS19 <barbara_sabino@estudante.sesisenai.org.br>
2024-06-11 11:35:55 -0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import './App.css'
-import React, { useCallback, useReducer, useState } from 'react'
+import React, {useCallback, useEffect, useReducer, useState} from 'react'
 
-const taskReducer = (state, action) => {
-  switch (action.type) {
+const taskReducer = (state, action) =>{
+  switch(action.type){
     case 'ADD_TAREFA':
-      return [...state, action.payload]
+      return[...state, action.payload]
     case 'CONCLUIDO':
       const atualizarTarefa = [...state]
-      atualizarTarefa[action.payload].complete = true
+      atualizarTarefa[action.payload].completed = true
       return atualizarTarefa
-
+    case 'EXCLUIR' :
+    return state.filter((_, index) => index !== action.payload) 
+    default:
+      return state;
   }
 }
 
@@ -17,47 +20,52 @@ function App() {
   const [tarefa, setTarefa] = useState('')
 
   const [tarefaAtual, dispatch] = useReducer(taskReducer, [])
+  const addTarefa = useCallback(() =>{
 
-  const addTarefa = useCallback(() => {
-    if (tarefa.trim() !== '') {
-      dispatch({ type: 'ADD_TAREFA', payload: { text: tarefa, completed: false } })
-      setTarefa('')
+    if(tarefa.trim() !== ''){
+      dispatch({type: 'ADD_TAREFA', payload: {text: tarefa, completed: false}})
+      setTarefa('');
     }
   }, [tarefa])
 
- const concluirTarefa = useCallback((index) =>{
-  dispatch ({type: 'CONCLUIDO', payload: index})
+  const concluirTarefa = useCallback((index) =>{
+    dispatch ({type: 'CONCLUIDO', payload: index})
+  })
 
-})
-  return (
+  const excluirTarefa = useCallback((index) =>{
+    dispatch({type: 'EXCLUIR', payload: index})
+  })
+
+    return (
     <>
       <div className="center">
         <h1>Lista de Tarefas</h1>
         <div className="input">
-          <input type="text" placeholder="Nova Tarefa" value={tarefa} onChange={(e) => setTarefa(e.target.value)}>
-          </input>
+          <input type="text" 
+            placeholder='Nova tarefa'
+            value={tarefa}
+            onChange={(e) => setTarefa(e.target.value)}
+          />
           <button onClick={addTarefa}>Adicionar</button>
         </div>
         <ul>
           {tarefaAtual.map((tarefas, index) => (
             <li key={index}>
               <span style={{ textDecoration: tarefas.completed ? 'line-through' : 'none' }}>
-                {tarefas.text}
-                {console.log(tarefas.completed)}
+                 {tarefas.text}
               </span>
               {
                 !tarefaAtual.completed && (
                   <>
-                    <button onClick={() => concluirTarefa(index)}>
-                      {console.log(index)}
+                    <br/><button onClick={() => concluirTarefa(index)}>
                       Concluir tarefa
                     </button>
+                   <button onClick={() => excluirTarefa(index)}>Excluir tarefa</button>
                   </>
                 )
               }
             </li>
-          )
-          )}
+          ))}
         </ul>
       </div>
     </>
